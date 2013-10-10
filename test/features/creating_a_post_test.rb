9f5d2c08de74fa5771c88bfb5edec9522d501a10
Tina Kumar <tinakumar07@gmail.com>
2013-10-09 23:50:20 -0700

    added fixtures

--- test/features/creating_a_post_test.rb
+++ test/features/creating_a_post_test.rb
@@ -2,12 +2,14 @@ require "test_helper"
 
 feature "CreatingAPost" do
   scenario "creating a new post" do
-    visit posts_path
-    click_on 'New Post'
-    fill_in 'Title', with: 'Space, the final frontier'
-    fill_in 'Content', with: "These are the adventures of the starship enterprise!"
+    visit new_post_path
+    #click_on 'New Post'
+    #fill_in 'Title', with: 'Space, the final frontier'
+    #fill_in 'Content', with: "These are the adventures of the starship enterprise!"
+    fill_in 'Title', with: posts(:ta).title
+    fill_in 'Content', with: posts(:ta).content
     click_on 'Create Post'
-    page.text.must_include 'Space, the final frontier'
+    page.text.must_include posts(:ta).title
     page.text.must_include 'Post was successfully created'
   end
 end
--- test/features/deleting_a_post_test.rb
+++ test/features/deleting_a_post_test.rb
@@ -5,11 +5,11 @@ feature "DeletingAPost" do
 
     #page.must_have_content "Hello World"
     #page.wont_have_content "Goobye All!"
-    post = Post.create(title: "Becoming a Code Fellow", content: "Means striving for excellence.")
-    visit posts_path(post)
+    post = Post.create(posts(:ta).title, posts(:ta).content)
+    visit posts_path#posts_path(post)
     #select a post to destroy
-    page.find("tr:last").click_on "Destroy"
-    #click_on "Destroy"
+    page.find(posts(:ta).title)#.click_on "Destroy"
+    click_on "Destroy"
 
     page.text.must_include 'Post was successfully deleted'
   end
