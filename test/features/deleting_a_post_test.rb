376e657f106ce3c0b567f5be64ad94601e012564
Tina Kumar <tinakumar07@gmail.com>
2013-10-10 12:36:03 -0700

    fixed deleting a post test

--- test/features/deleting_a_post_test.rb
+++ test/features/deleting_a_post_test.rb
@@ -5,12 +5,12 @@ feature "DeletingAPost" do
 
     #page.must_have_content "Hello World"
     #page.wont_have_content "Goobye All!"
-    post = Post.create(posts(:ta).title, posts(:ta).content)
+    #post = Post.create(posts(:ta).title, posts(:ta).content)
     visit posts_path#posts_path(post)
     #select a post to destroy
-    page.find(posts(:ta).title)#.click_on "Destroy"
-    click_on "Destroy"
+    page.find("tr:last").click_on "Destroy"
+    #click_on "Destroy"
 
-    page.text.must_include 'Post was successfully deleted'
+    page.wont_have_content 'Rainbows'
   end
 end
